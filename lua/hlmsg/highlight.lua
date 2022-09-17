666de1d1ee2d8e7bb26a7364257c48c3c54a4cbc
notomo <notomo.motono@gmail.com>
2022-09-17 06:27:50 +0000

    Fix to use attr_id

--- lua/hlmsg/highlight.lua
+++ lua/hlmsg/highlight.lua
@@ -1,14 +1,55 @@
 local M = {}
 
+-- HACK
+
+local ffi = require("ffi")
+local setup_ffi = function()
+  -- The following cdef code is from https://github.com/neovim/neovim/blob/c126c1f73a0bae3ac40ce460ef303348a671a08f/src/nvim/highlight_defs.h .
+  -- Its license is https://www.apache.org/licenses/LICENSE-2.0 .
+  ffi.cdef([[
+typedef int32_t RgbValue;
+typedef struct attr_entry {
+  int16_t rgb_ae_attr, cterm_ae_attr;
+  RgbValue rgb_fg_color, rgb_bg_color, rgb_sp_color;
+  int cterm_fg_color, cterm_bg_color;
+  int hl_blend;
+} HlAttrs;
+HlAttrs syn_attr2entry(int attr);
+]])
+end
+local ok, err = pcall(setup_ffi)
+if not ok and not err:find("redefine") then
+  error(err)
+end
+
+function M._attr_id_to_hl_group(attr_id)
+  local attributes = ffi.C.syn_attr2entry(attr_id)
+  local hl_group = "HlmsgAttribute" .. tostring(attr_id)
+
+  local blend = attributes.hl_blend
+  if blend == -1 then
+    blend = nil
+  end
+
+  vim.api.nvim_set_hl(0, hl_group, {
+    fg = attributes.rgb_fg_color,
+    bg = attributes.rgb_bg_color,
+    bold = bit.band(attributes.rgb_ae_attr, 0x02),
+    blend = blend,
+  })
+  return hl_group
+end
+
 function M.add(ns, bufnr, entries)
   for i, entry in ipairs(entries) do
     local _, contents = unpack(entry)
     local start_col = 0
     for _, pair in ipairs(contents) do
-      local hl_id, text = unpack(pair)
+      local attr_id, text = unpack(pair)
+      local hl_group = M._attr_id_to_hl_group(attr_id)
       local end_col = start_col + vim.fn.strdisplaywidth(text)
       vim.api.nvim_buf_set_extmark(bufnr, ns, i - 1, start_col, {
-        hl_group = hl_id,
+        hl_group = hl_group,
         end_col = end_col,
       })
       start_col = end_col
