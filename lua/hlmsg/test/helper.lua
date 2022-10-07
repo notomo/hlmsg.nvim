a364c105a629b8e46168d2a34bf93e2b4b06168c
notomo <notomo.motono@gmail.com>
2022-10-07 12:28:52 +0000

    Remove unused test helper

--- lua/hlmsg/test/helper.lua
+++ lua/hlmsg/test/helper.lua
@@ -14,22 +14,4 @@ function helper.after_each()
   helper.cleanup_loaded_modules(plugin_name)
 end
 
-function helper.on_finished()
-  local finished = false
-  return setmetatable({
-    wait = function()
-      local ok = vim.wait(1000, function()
-        return finished
-      end, 10, true)
-      if not ok then
-        error("wait timeout")
-      end
-    end,
-  }, {
-    __call = function()
-      finished = true
-    end,
-  })
-end
-
 return helper
