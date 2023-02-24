a2fabfcf398898338223a4719b7f621529db0575
notomo <notomo.motono@gmail.com>
2023-02-24 09:38:33 +0900

    Add type doc

--- lua/hlmsg/init.lua
+++ lua/hlmsg/init.lua
@@ -6,17 +6,19 @@ function M.render(bufnr)
   require("hlmsg.command").render(bufnr)
 end
 
+--- @class HlmsgMessage
+--- @field kind string: |ui-messages|'s kind
+--- @field line string: message splitted by newline
+--- @field chunks HlmsgChunk[] |HlmsgChunk|
+
+--- @class HlmsgChunk
+--- @field text string chunked text
+--- @field hl_group string highlight group
+--- @field start_col integer 0-based index column
+--- @field end_col integer 0-based index column
+
 --- Returns highlighted text chunks.
---- @return table: {
----   kind = string,
----   line = string,
----   chunks = {
----     text = string,
----     hl_group = string,
----     start_col = number,
----     end_col = number
----   }[]
---- }[]
+--- @return HlmsgMessage[]: |HlmsgMessage|
 function M.get()
   return require("hlmsg.command").get()
 end
